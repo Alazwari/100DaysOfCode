945b9854b21a98f6897da8fe6f9f53cb509d588c
aazwri <aazwri@gmail.com>
2019-08-20 18:23:50 +0300

    upload day001.py

---
+++ day001.py
@@ -0,0 +1,11 @@
+#Day 001
+
+macbook = "\U0001F4BB"
+smile = "\U0001f604"
+print("")
+for i in range(4):
+    print(smile, end="")
+    if i == 3:
+       print(smile) 
+print("Hello World,\nthis is the first day of #100DaysOfCode", macbook,"\n#المبادرة_السعودية_للمطورين", macbook, "\n#100 _يوم_برمجة",macbook)
+print("")
