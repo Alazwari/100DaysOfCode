7caddc2c7a3d771e769b0c8eef5603df742bda4f
aazwri <aazwri@gmail.com>
2019-08-21 09:27:41 +0300

    upload day002.py

---
+++ day002.py
@@ -0,0 +1,19 @@
+#Day 002
+
+"""
+غالبا نستخدم التعليقات لشرح ووضع مالحظات حول الكود
+نستخدم التعليقات أيضا لتسهيل عملية فهم وقراءة الكود، خصوصا لو أردنا مراجعته بعد وقت طويل
+يمكنك استخدام التعليقات لألكواد أيضا في حين ال نريدها أن تتنفذ أثناء عملية التشغيل/التنفيذ
+"""
+
+keyboaed = "\U00002328"
+pen = "\U0001F4DD"
+hashCode = "\U00000023"
+
+print("\n")
+print("""To write {2}  comments {0} in Python we use {1}, or we can add a multiline string (triple quotes) in your code and place your comment inside it 
+\"\"\"
+Like this comment {0},
+this {0}
+and this {0}
+\"\"\"""".format(pen, hashCode, keyboaed))
