8c7abaad323d8b1371d3ace798ab7376d7907a64
aazwri <aazwri@gmail.com>
2019-08-27 23:13:20 +0300

    update day008.py

--- day008.py
+++ day008.py
@@ -1,36 +1,33 @@
 #Day 8
 
 #Rock Paper Scissors Game
-from random import randint
-computer = randint(0,2)
-player = input('Player #1, your turn to play:  ').lower()
-
-
-if computer == 0:
-    computer_choice = 'rock'
-elif computer == 1:
-    computer_choice = 'paper'
-else:
-    computer_choice = 'scissors'
-
-print(f'Computer plays {computer_choice}')
-
-if player == computer_choice:
-    print('Tie!')
-elif player == 'rock':
-    if computer_choice == 'scissors':
-        print('You Wins!')
+while True:
+    from random import randint
+    computer = randint(0,2)
+    player = input('Player #1, your turn to play:  ').lower()
+    if computer == 0:
+        computer_choice = 'rock'
+    elif computer == 1:
+        computer_choice = 'paper'
     else:
-        print('Computer Wins!')
-elif player == 'paper':
-    if computer_choice == 'rock':
-        print('You Wins!')
+        computer_choice = 'scissors'
+    print(f'Computer plays {computer_choice}')
+    if player == computer_choice:
+        print('Tie!')
+    elif player == 'rock':
+        if computer_choice == 'scissors':
+            print('You Win!')
+        else:
+            print('Computer Wins!')
+    elif player == 'paper':
+        if computer_choice == 'rock':
+            print('You Win!')
+        else:
+            print('Computer Wins!')
+    elif player == 'scissors':
+        if computer_choice == 'rock':
+            print('Computer Wins!')
+        else:
+            print('You Win!')
     else:
-        print('Computer Wins!')
-elif player == 'scissors':
-    if computer_choice == 'rock':
-        print('Computer Wins!')
-    else:
-        print('You Wins!')
-else:
-    print('You Enter A Wrong Word')
+        print('You Enter A Wrong Word')
