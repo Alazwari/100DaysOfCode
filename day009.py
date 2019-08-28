452124ef869ea50882a199ca60bf9b955a8c352e
aazwri <aazwri@gmail.com>
2019-08-28 10:37:08 +0300

    upload day009.py

---
+++ day009.py
@@ -0,0 +1,13 @@
+#Day 9
+
+name  = input('Enter Your Name: ')
+def line():
+    print("-----------------")
+
+
+line()
+print("Hello {}, this is the day number 10 in Saudi Initiative for Developers".format(name))
+
+#in python 3.7 we use f'' to format the string
+line()
+print(f"Hello {name}, this is the day number 10 in Saudi Initiative for Developers")
