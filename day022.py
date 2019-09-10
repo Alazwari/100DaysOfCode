22907dedbce3ff5567193cf7a12115635f14fbb8
aazwri <aazwri@gmail.com>
2019-09-10 22:08:37 +0300

    upload day022.py

---
+++ day022.py
@@ -0,0 +1,12 @@
+#Day 22
+
+numbers = {n: ('even' if n%2==0 else 'odd') for n in range(1,20)}
+print(numbers)
+
+####
+print('---------------')
+####
+
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+total_donations = sum(donations.values())
+print(total_donations)
