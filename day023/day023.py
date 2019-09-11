3329fb7685108d101ae8d079b282837020b5de3e
aazwri <aazwri@gmail.com>
2019-09-11 21:27:55 +0300

    upload day023

--- day023/day023.py
+++ day023/day023.py
@@ -1,12 +1,25 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 
 
+
+
+class Calc(GridLayout):
+    def pressed(self, txt):
+        self.ids.tx.text += txt
+        print(self.ids.tx.text)
+    def calculation(self, calculation):
+        if calculation:
+            try:
+                self.ids.tx.text = str(eval(calculation))
+            except Exception:
+                self.ids.tx.text = 'Error'
 
 class Day023App(App):
-    def pressed(self):
-        self.ids.txt =
+    def build(self):
+        return Calc()
 
 if __name__=='__main__':
     Day023App().run()
