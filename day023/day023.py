1ccfbaacc28e0a9a8112a0e142855647d7ffafc0
aazwri <aazwri@gmail.com>
2019-09-11 20:35:40 +0300

    created a folder for each day, and update README.md

---
+++ day023/day023.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+
+
+class Day023App(App):
+    def pressed(self):
+        self.ids.txt =
+
+if __name__=='__main__':
+    Day023App().run()
