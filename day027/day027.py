4d9f8d8c8c04dc9e5a86d93f7c2aff4d48ddbb7a
aazwri <aazwri@gmail.com>
2019-09-15 22:13:01 +0300

    upload day027

---
+++ day027/day027.py
@@ -0,0 +1,19 @@
+#Day 27
+
+total = 100
+#country = "US"
+country = input('Enter Your Conuntry: ').upper()
+if country == "SA":
+    if total <= 50:
+        print("Shipping Cost is  $50")
+elif total <= 100:
+        print("Shipping Cost is $25")
+elif total <= 150:
+	    print("Shipping Costs $5")
+else:
+        print("FREE")
+if country == "US": 
+	  if total <= 50:
+	    print("Shipping Cost is  $100")
+else:
+	    print("FREE")
