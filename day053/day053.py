42e2488a32ac0d175d63879603cec1a443b01066
aazwri <aazwri@gmail.com>
2019-10-22 23:18:41 +0300

    upload day53

---
+++ day053/day053.py
@@ -0,0 +1,14 @@
+# Day 53
+import my_math
+import datetime
+
+my_math.op.sum(1,8)
+my_math.op.sub(4,3)
+my_math.op.mul(6,6)
+my_math.op.div(8,2)
+
+
+print(datetime.datetime.now().year)
+print(datetime.datetime.now())
+print(datetime.datetime.now().month)
+print(datetime.datetime.now().day)
---
+++ day053/my_math.py
@@ -0,0 +1,10 @@
+# Day 53
+class op:
+    def sum(x,y):
+        print(x+y)
+    def sub(x,y):
+        print(x-y)
+    def mul(x,y):
+        print(x*y)
+    def div(x,y):
+        print(x/y)
