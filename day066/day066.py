26ce05ac6416159c5997d5765d4780824f807b58
aazwri <aazwri@gmail.com>
2019-10-25 16:50:46 +0300

    upload day067

---
+++ day066/day066.py
@@ -0,0 +1,7 @@
+# Day 66
+
+quantity = 3
+itemno = 567
+price = 50
+text = 'I want {0} pieces of item number {1} for {2:.2f} dollars.'
+print(text.format(quantity, itemno, price))
---
+++ day067/day067.py
@@ -0,0 +1,4 @@
+# Day 67
+
+name = input('Please enter the first, and the second letter of your name : ')
+print(f'Your name is begin with {name[0]}, and end with {name[1]}')
