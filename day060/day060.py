3fed52b5559d342e601d101b24b0e2acdae602e9
aazwri <aazwri@gmail.com>
2019-10-25 14:28:41 +0300

    upload day060

---
+++ day060/day060.py
@@ -0,0 +1,9 @@
+# Day 60
+import json
+days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+print(days)
+
+days = json.dumps(days)
+print(type(days))
+print(days)
