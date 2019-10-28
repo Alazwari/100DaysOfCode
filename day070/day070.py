f78aa9844197b1527a0f5e76181d4c7f069488e5
alazwari <aazwri@gmail.com>
2019-10-29 00:25:57 +0300

    update day070

---
+++ day070/day070.py
@@ -0,0 +1,17 @@
+# Day 70
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'python',
+    password = 'python',
+    database = 'day070'
+)
+
+mycrusor = mydb.cursor()
+#mycrusor.execute("CREATE TABLE customers (ID INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255), address VARCHAR(255))")
+mycrusor.execute('DESC customers')
+for x in mycrusor:
+    print(x)
+
